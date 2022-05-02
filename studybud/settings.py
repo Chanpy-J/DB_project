0baeae0aad8c4a79c18fd169ce6ff48a94f26b67
Eugene Lan <eugenelan@Eugenes-MBP.lan>
2022-05-02 12:39:52 -0400

    database

--- studybud/settings.py
+++ studybud/settings.py
@@ -92,6 +92,16 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'project',
+#         'USER':'root',
+#         'PASSWORD':'Eugene0918',
+#         'HOST':'127.0.0.1',
+#         'PORT':'3306'
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
